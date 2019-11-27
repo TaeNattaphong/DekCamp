8e7800fc1c45b350bc62eee0ce4a62410b701f7e
TaeNattaphong <nattaphong.r@ku.th>
2019-11-28 06:11:06 +0700

    detail camp1

--- app/src/main/java/com/example/dekcamp/CampDetailActivity.kt
+++ app/src/main/java/com/example/dekcamp/CampDetailActivity.kt
@@ -1,12 +1,37 @@
 package com.example.dekcamp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class CampDetailActivity : AppCompatActivity() {
 
+    lateinit var voteButton: Button
+    var state: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camp_detail)
+
+        voteButton = findViewById(R.id.voteButton)
+    }
+
+    fun onClickedVote(view: View) {
+        state=!state
+        if (state) {
+            voteButton.setBackgroundColor(Color.parseColor("#FFC200"))
+            voteButton.setTextColor(Color.parseColor("#E60000"))
+            //vote ++
+
+        } else {
+            voteButton.setBackgroundColor(Color.parseColor("#E0D9D9"))
+            voteButton.setTextColor(Color.BLACK)
+        }
+
+
+
+        //#E0D9D9  #FFC200
+
     }
 }
